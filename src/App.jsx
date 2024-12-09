7d794d8ebd42040ad337f0ac20ed4e05153976ff
Jhon Mark <jhonmarktamayo134@gmail.com>
2024-12-09 22:51:53 +0700

    Javascript if statement

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
 import './App.css'
 
+function List(props) {
+  if (!props.animalsList) {
+    return <div>Loading...</div>;
+  }
+
+  if (props.animalsList.length === 0) {
+    return <div>There are no animals in the list!</div>;
+  }
+
+  return (
+    <ul>
+      {props.animalsList.map((animal) => {
+        return <li key={animal}>{animal}</li>;
+      })}
+    </ul>
+  )
+}
+
 function App() {
-  const animals = ["Lion", "Cow", "Snake", "Lizard"];
-  const animalList = animals.map((animal) => <li key={animal}>{animal}</li>)
+  const animals = ["Toger"];
 
   return (
     <div>
-      <h1>Animals:</h1>
-      <ul>
-        {animalList}
-      </ul>
+      <h1>Animals: </h1>
+      <List animalsList={animals} />
     </div>
   )
 }
-export default App
+
+export default App;
