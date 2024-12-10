743802b5101e04fcd71a24f5b306db55743de7fb
Jhon Mark <jhonmarktamayo134@gmail.com>
2024-12-10 21:52:23 +0700

    Adding function list that use filter and map

--- src/Greeting.jsx
+++ src/Greeting.jsx
@@ -1,5 +1,55 @@
-function Greeting() {
-    return <h1>&quot;Praxis website.&quot;</h1>;
+function List() {
+    const people = [{
+
+        id: 0,
+        name: 'Creola Katherine Johnson',
+        profession: 'mathematician',
+        accomplishment: 'spaceflight calculations',
+        imageId: 'MK3eW3A'
+      }, {
+        id: 1,
+        name: 'Mario José Molina-Pasquel Henríquez',
+        profession: 'chemist',
+        accomplishment: 'discovery of Arctic ozone hole',
+        imageId: 'mynHUSa'
+      }, {
+        id: 2,
+        name: 'Mohammad Abdus Salam',
+        profession: 'physicist',
+        accomplishment: 'electromagnetism theory',
+        imageId: 'bE7W1ji'
+      }, {
+        id: 3,
+        name: 'Percy Lavon Julian',
+        profession: 'chemist',
+        accomplishment: 'pioneering cortisone drugs, steroids and birth control pills',
+        imageId: 'IOjWm71'
+      }, {
+        id: 4,
+        name: 'Subrahmanyan Chandrasekhar',
+        profession: 'astrophysicist',
+        accomplishment: 'white dwarf star mass calculations',
+        imageId: 'lrWQx8l'
+      }];
+      
+    const chemist = people.filter(person =>
+        person.profession === 'chemist'
+    );
+    const listItems = chemist.map(person =>
+        <li key={person.id}>
+            <img
+                src={'https://i.imgur.com/' + person.imageId + 's.jpg'}
+                alt={person.name}
+            />
+            <p>
+                <b>{person.name}:</b>
+                {'' + person.profession + ''}
+                known for (person.accomplishment)
+            </p>
+        </li>
+    );
+    return <ul>{listItems}</ul>
+    
 }
 
-export default Greeting;
+export default List;
